c99dc636cd56050d9725f65ba32791f1c6296452
roopapa <rooria24@gmail.com>
2021-12-09 14:32:43 +0900

    プレイヤーが方向キーまたはADキーを入力すると左右移動する機能の作成

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public enum DIRECTION
+    {
+        STOP,
+        RIGHT,
+        LEFT,
+    }
+
+    DIRECTION direction = DIRECTION.STOP;
+    [SerializeField] Rigidbody2D rigidbody2D;
+    float _speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var _x_val = Input.GetAxis("Horizontal");
+
+        if (_x_val == 0)
+        {
+            direction = DIRECTION.STOP;
+        }
+        else if (_x_val > 0)
+        {
+            direction = DIRECTION.RIGHT;
+        }
+        else if (_x_val < 0)
+        {
+            direction = DIRECTION.LEFT;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        switch (direction)
+        {
+            case DIRECTION.STOP:
+                _speed = 0;
+                break;
+            case DIRECTION.RIGHT:
+                _speed = 3;
+                transform.localScale = new Vector3(1, 1, 1);
+                break;
+            case DIRECTION.LEFT:
+                _speed = -3;
+                transform.localScale = new Vector3(-1, 1, 1);
+                break;
+        }
+        rigidbody2D.velocity = new Vector2(_speed, rigidbody2D.velocity.y);
+    }
+}
